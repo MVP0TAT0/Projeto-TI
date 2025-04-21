5fe1a855c38dbdfc63bb3ec3699509c72ccbaad8
MVP0TAT0 <rodrigoaaclr@hotmail.com>
2025-04-21 21:16:44 +0100

    Updated commentaries

--- Escravo/Escravo.ino
+++ Escravo/Escravo.ino
@@ -1,12 +1,21 @@
+// ---------------------
+// Bibliotecas
+// ---------------------
 #include <Adafruit_NeoPixel.h>  // Biblioteca para controlar neopixel
 #include <SoftwareSerial.h>     // Biblioteca para melhor controlo de serial communication
 
+// ---------------------
+// Definições de pinos
+// ---------------------
 #define LED_PIN 5     // Fita LED
 #define NUM_LEDS 8    // Número de Leds da fita
 #define LED_NORMAL 4  // LED normal no pino 4 (para simular outra fita que nao temos ainda)
 #define TRIG_PIN 2    // Trigger ultrassonico
 #define ECHO_PIN 3    // Echo ultrassonico
 
+// ---------------------
+// Medir distância com o sensor ultrassónico
+// ---------------------
 long readDistanceCM() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -19,65 +28,91 @@ long readDistanceCM() {
   return distance;
 }
 
+// ---------------------
+// Inicializações globais
+// ---------------------
+SoftwareSerial mySerial(8, 7);  // Iniciar a Serial Communication via SoftwareSerial com pinos digitais
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);   // Iniciar a fita LED
 
-SoftwareSerial mySerial(8, 7);  // RX, TX
-Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+// Variáveis de animação dos LEDs
 bool animationRunning = false;  // Verifica se a animação está a correr
 int position = 0;               // Posição dos leds da animação
 
+// ---------------------
+// Setup
+// ---------------------
 void setup() {
-  pinMode(LED_NORMAL, OUTPUT);  // LED normal
+
+  // LED normal apenas para testar (não havendo mais fitas LED)
+  pinMode(LED_NORMAL, OUTPUT);
   digitalWrite(LED_NORMAL, LOW);
+
+  // Sensor ultrassónico
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
+  // Fita LED
   strip.begin();
   strip.setBrightness(26);  // Brilho default
   strip.show();
 
+  // Serial Communication
   mySerial.begin(9600);
   Serial.begin(9600);
 }
 
+// ---------------------
+// Loop
+// ---------------------
 void loop() {
+
+  // Se receber o comando do Mestre, executa este código
   if (mySerial.available()) {
     String comando = mySerial.readStringUntil('\n');
     comando.trim();
     Serial.println("Recebido: [" + comando + "]");  // Debug para ver se está o Escravo está a receber os dados do Mestre
 
+    // Se recebe o comando NEO (neopixel) e a animação não estiver a correr, executa este código
     if (comando == "NEO" && !animationRunning) {
       animationRunning = true;
       position = 0;
-    } else if (comando == "LED") {
+    } else if (comando == "LED") {    // Se receber o comando LED do outro botão, acende o LED normal
       digitalWrite(LED_NORMAL, HIGH);
       delay(10);  // LED acende por 0.5s
       digitalWrite(LED_NORMAL, LOW);
     }
   }
 
+  // Se a animação LED estiver a correr, executa o seguinte
   if (animationRunning) {
-    strip.clear();
+    strip.clear();  // Limpa os LEDs
 
+    // Acende os 3 primeiros LEDs
     for (int i = 0; i < 3; i++) {
       int ledIndex = position + i;
       if (ledIndex < NUM_LEDS) {
-        strip.setPixelColor(ledIndex, strip.Color(255, 0, 155));
+        strip.setPixelColor(ledIndex, strip.Color(255, 0, 155));  // Cor rosa
       }
     }
 
     // Medir distância e ajustar brilho
     long distance = readDistanceCM();
+
+    // Debugging
     Serial.print("Distância: ");
     Serial.print(distance);
     Serial.println(" cm");
-    int brightness = map(constrain(distance, 5, 100), 5, 100, 0, 255);  // Mapear 5-100cm → 10-255 brilho
+
+    //Mapear 5-100cm → 10-255 brilho
+    int brightness = map(constrain(distance, 5, 100), 5, 100, 0, 255);  
     strip.setBrightness(brightness);
 
+    // Atualiza os LEDs
     strip.show();
     delay(50);
     position++;
 
+    // Termina a animação quando atinge o fim da fita
     if (position > NUM_LEDS) {
       animationRunning = false;
     }
--- Mestre/Mestre.ino
+++ Mestre/Mestre.ino
@@ -1,20 +1,44 @@
-#include <SoftwareSerial.h>	// Biblioteca para melhor controlo de serial communication
+// ---------------------
+// Bibliotecas
+// ---------------------
+#include <SoftwareSerial.h>  // Biblioteca para melhor controlo de serial communication
 
-int botoes[] = { 9, 8, 7, 6, 5, 4, 3 };	// Pinos dos botões
-int piezo = 2;													// Pino piezo buzzer
-int potPin = A0;												// Pino potenciómetro
+// ---------------------
+// Definições de pinos
+// ---------------------
+int botoes[] = { 9, 8, 7, 6, 5, 4, 3 };  // Pinos dos botões
+int piezo = 2;                           // Pino piezo buzzer
+int potPin = A0;                         // Pino potenciómetro
 
-float notas[] = { 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88 };	// Frequências das notas
+// ---------------------
+// Notas musicais
+// ---------------------
+float notas[] = {
+	261.63,  // Dó
+	293.66,  // Ré
+	329.63,  // Mi
+	349.23,  // Fá
+	392.00,  // Sol
+	440.00,  // Lá
+	493.88   // Si — Iremos remover futuramente porque só iremos ter 6 teclas
+};
 
-SoftwareSerial ledSerial(10, 11); // RX, TX (Sendo o mestre, só usa TX)
+// ---------------------
+// Comunicação serial com o escravo (NeoPixel)
+// ---------------------
+SoftwareSerial ledSerial(10, 11);  // RX, TX (Sendo o mestre, só usa TX, ou seja, transmite)
 
+// ---------------------
+// Setup
+// ---------------------
 void setup() {
 
-	// Setup dos botões usando as resistências interiores
+	// Setup dos botões usando as resistências internas
 	for (int i = 0; i < 7; i++) {
 		pinMode(botoes[i], INPUT_PULLUP);
 	}
-	
+
+	// Buzzer
 	pinMode(piezo, OUTPUT);
 
 	// Setup de Serial Communication
@@ -22,20 +46,31 @@ void setup() {
 	Serial.begin(9600);
 }
 
+// ---------------------
+// Loop
+// ---------------------
 void loop() {
 	bool notaTocada = false;
 
+	// Se algum dos botões for pressionar, executa o seguinte código
 	for (int i = 0; i < 7; i++) {
 		if (digitalRead(botoes[i]) == LOW) {
-			float pot = analogRead(potPin); // Valor entre 0–1023
-			float pitchFactor = map(pot, 0, 1023, 50, 150); // Fator entre 50% e 150%
-			float frequencia = notas[i] * (pitchFactor / 100.0); // Ajuste da nota base
+			// Leitura do potenciómetro
+			float pot = analogRead(potPin);                       // Valor entre 0–1023
 
-			tone(piezo, frequencia, 250); // Toca nota ajustada por 250ms
+			// Mapear o valor do potenciómetro para um pitch entre 50% a 150%
+			float pitchFactor = map(pot, 0, 1023, 50, 150);
 
-			if (i == 0) { // Botão 9 → NEO
+			// Ajustar frequência da nota (o som fica baixo sem isto)
+			float frequencia = notas[i] * (pitchFactor / 100.0);
+
+			// Toca a nota
+			tone(piezo, frequencia, 250);  // Toca nota ajustada por 250ms
+
+			// Enviar comandos via Serial Communication, dependendo do botão
+			if (i == 0) {  								// Primeiro botão envia comando para ligar a fita Neopixel
 				ledSerial.println("NEO");
-			} else if (i == 1) { // Botão 8 → LED
+			} else if (i == 1) {  				// Segundo botão envia comando para ligar o LED normal (não temos mais fitas neopixel por enquanto)
 				ledSerial.println("LED");
 			}
 
@@ -44,6 +79,7 @@ void loop() {
 		}
 	}
 
+	// Se o botão não está a ser tocado, desliga o som do piezo buzzer
 	if (!notaTocada) {
 		noTone(piezo);
 	}
